220718f04c92adcf28b98f49cb53fdfb0c29625f
SeregaZH <zhelezko1990@gmail.com>
2015-12-07 23:56:02 +0300

    add tests for collection required attribute

---
+++ FluentValidation.Tests/Validators/CollectionRequiredValidatorTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using FluentValidation.Validation.Models.Results;
+using FluentValidation.Validation.Validators;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentValidation.Tests.Validators
+{
+    [TestClass]
+    public class CollectionRequiredValidatorTests
+    {
+        [TestMethod]
+        public void TestCollectionRequiredValidatorWithNullCollection()
+        {
+            CollectionRequiredValidatorTemplate(
+                validationTargetFactory: () => new FakeValidationTargetModel(),
+                propertyToValidate: x => x.CollectionValidationTargetProperty,
+                asserts: result =>
+                {
+                    Assert.IsFalse(result.IsValid());
+                    Assert.IsInstanceOfType(result, typeof(PropertyValidationResult));
+                    Assert.AreEqual(((PropertyValidationResult)result).PropertyName,
+                        nameof(FakeValidationTargetModel.CollectionValidationTargetProperty));
+                });
+        }
+
+        [TestMethod]
+        public void TestCollectionRequiredValidatorWithEmptyCollection()
+        {
+            CollectionRequiredValidatorTemplate(
+                validationTargetFactory: () => new FakeValidationTargetModel { CollectionValidationTargetProperty = new List<object>() },
+                propertyToValidate: x => x.CollectionValidationTargetProperty,
+                asserts: result =>
+                {
+                    Assert.IsFalse(result.IsValid());
+                    Assert.IsInstanceOfType(result, typeof(PropertyValidationResult));
+                    Assert.AreEqual(((PropertyValidationResult)result).PropertyName,
+                        nameof(FakeValidationTargetModel.CollectionValidationTargetProperty));
+                });
+        }
+
+        [TestMethod]
+        public void TestCollectionRequiredValidatorWithNotEmptyCollection()
+        {
+            CollectionRequiredValidatorTemplate(
+                validationTargetFactory: () => new FakeValidationTargetModel { CollectionValidationTargetProperty = new List<object> { new object() } },
+                propertyToValidate: x => x.CollectionValidationTargetProperty,
+                asserts: result =>
+                {
+                    Assert.IsTrue(result.IsValid());
+                    Assert.IsInstanceOfType(result, typeof(PropertyValidationResult));
+                    Assert.AreEqual(((PropertyValidationResult)result).PropertyName,
+                        nameof(FakeValidationTargetModel.CollectionValidationTargetProperty));
+                });
+        }
+
+        private void CollectionRequiredValidatorTemplate<TModel, TProperty>(
+            Func<TModel> validationTargetFactory,
+            Expression<Func<TModel, IEnumerable<TProperty>>> propertyToValidate,
+            Action<ValidationResult> asserts)
+        {
+            var validationTarget = validationTargetFactory();
+            var validatorDescriptor = ValidatorTestHelper.CreateDefaultValidationDescriptor();
+            var validator = new CollectionRequiredValidator<TModel, TProperty>(validatorDescriptor, 0,
+                propertyToValidate);
+            var validationResult = validator.Validate(validationTarget);
+            asserts(validationResult);
+        }
+    }
+}
---
+++ FluentValidation.Tests/Validators/ValidatorTestHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.Validation.Models;
+
+namespace FluentValidation.Tests.Validators
+{
+    public static class ValidatorTestHelper
+    {
+        public static ValidatorDescriptor CreateDefaultValidationDescriptor()
+        {
+            return new ValidatorDescriptor(Guid.NewGuid(), "TestRule", "Error", "Descriptor");
+        }
+    }
+}
