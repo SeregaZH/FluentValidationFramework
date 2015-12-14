babeccaf7092e8034eb45efb234ca55f3046a5b1
SeregaZH <zhelezko1990@gmail.com>
2015-12-15 00:37:47 +0300

    add validation config

---
+++ FluentValidation/Validation/Configuration/ValidatorsConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FluentValidation.Validation.Configuration
+{
+    public class ValidatorsConfig<TModel>
+    {
+        public ValidatorsConfig(string rulesetName, IEnumerable<IValidator<TModel>> validators)
+        {
+            RulesetName = rulesetName;
+            Validators = validators;
+        }
+
+        public string RulesetName { get; private set; }
+
+        public Type TargetType { get { return typeof(TModel); } }
+
+        public IEnumerable<IValidator<TModel>> Validators { get; private set; }
+    }
+}
